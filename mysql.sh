cf0e402783f7da325ade4490a570ce8fa49f2d3a
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-04-22 17:01:38 +0530

    db

---
+++ mysql.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+TIMESTAMP=$( date +%F-%H-%M-%S )
+SCRIPTNAME=$( echo $0 | cut -d "." -f1 )
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "please enter DB password"
+read mysql_root_expense
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+       echo -e "$2....$R FAILURE $N"
+    else
+       echo -e "$2....$G Success $N"
+    fi
+}
+USERID=$( id -u )
+if [ $USERID -ne 0 ]
+then
+    echo "please run with root access"
+    exit 1
+else
+    echo "your a root user"
+fi
+dnf install mysql-server -y &>> $LOGFILE
+VALIDATE "$?" "Installing mysql server"
+
+systemctl enable mysqld &>> $LOGFILE
+VALIDATE "$?" "Enabling mysql server"
+
+systemctl start mysqld &>> $LOGFILE
+VALIDATE "$?" "starting mysql server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE "$?" "setting up root password"
+
+
